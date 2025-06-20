40870a491a1e0e0921d46f73e8ba4b870500f70e
lee.minjin <lee_mimmi@naver.com>
2025-06-20 21:23:14 +0900

    Fix: simplify db.json path for Railway

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const jsonServer = require("json-server");
-const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, "db.json"));
+const router = jsonServer.router("db.json"); // 그냥 상대 경로로
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 4001;
 
 server.use(middlewares);
-server.use("/api", router); // 라우터 접두어 추가
+server.use("/api", router);
 
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
