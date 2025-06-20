42d2bf291be9d02b6d594fd75c51accfdfbdfd53
lee.minjin <lee_mimmi@naver.com>
2025-06-20 21:20:22 +0900

    Update server.js: add /api prefix to routes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const jsonServer = require("json-server");
+const path = require("path");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 4001;
 
 server.use(middlewares);
-server.use(router);
+server.use("/api", router); // 라우터 접두어 추가
+
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
